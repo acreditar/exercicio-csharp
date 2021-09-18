9d21e8846456c557f0f00e2134334f52ea4b441c
dissecar <brenocod100@gmail.com>
2021-09-17 22:23:02 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            //Chamar uma classe de outro arquivo: 
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            //comparaçao de idade: 
+            if (p1.Idade > p2.Idade) {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            } else {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+            
+        
+
+        }
+    }
+} 
---
+++ ConsoleApp1/pessoa.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp1 {
+    class Pessoa {
+        public string Nome;
+        public int Idade;
+
+    }
+}
